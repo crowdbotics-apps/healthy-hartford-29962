643a73706dfb34259ab54e746ef3681337f12fe7
Crowdbotics <team@crowdbotics.com>
2021-08-20 02:40:35 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/healthy_hartford_29962/MainActivity.java
@@ -0,0 +1,15 @@
+package com.healthy_hartford_29962;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "healthy_hartford_29962";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "healthy_hartford_29962Identifier",
+  url: "https://healthy_hartford_29962Identifier.botics.co",
+  api: "https://healthy_hartford_29962Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
