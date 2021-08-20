29de100203889ea8671dc36ea4616244748a9202
Crowdbotics <team@crowdbotics.com>
2021-08-20 02:42:45 +0000

    Update models

--- backend/healthy_hartford_29962/wsgi.py
+++ backend/healthy_hartford_29962/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthy_hartford_29962.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "healthy_hartford_29962.settings")
 
 application = get_wsgi_application()
